bc62ae0d8cf8225e8dbf150843c753f6d50574b5
dduenow <davidduenow@mwc-122205.dhcp.missouri.edu>
2017-11-09 12:51:40 -0600

    Initial Commit

---
+++ ParkingApp2/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  ParkingApp2
+//
+//  Created by David Duenow on 11/9/17.
+//  Copyright © 2017 David Duenow. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
