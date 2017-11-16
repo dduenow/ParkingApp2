6560baaed7bfbf96399d016d0c643e99346fb5bf
AbdulEldarrat <aoe7x5@mail.missouri.edu>
2017-11-16 13:35:02 -0600

    added notes to explain the desired functionality

--- ParkingApp2/listingViewController.swift
+++ ParkingApp2/listingViewController.swift
@@ -5,10 +5,37 @@
 //  Created by Abudy on 11/15/17.
 //  Copyright © 2017 David Duenow. All rights reserved.
 //
-
+//********************************** This page is for owners to list their spots on our platforms ***********************************//
 import UIKit
 
 class listingViewController: UIViewController {
-
+    
+    override func viewDidLoad(){
+        super.viewDidLoad()
+        
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        
+    }
+    
+    //Create list of listings
+    //Display list of listings
+    
+    //Create detail view of listing
+    //display detail view of listing
+    
+    //allow addition of spots
+        //Create new spot object
+        //fill data in
+        //upsert to cloud
+    //allow deletion of spots
+        //delete the object and upsert to cloud?
+    //allow editing of spots?
+        //pull down object from cloud
+        //change fields desired
+        //upsert new updated object to cloud
+    
     
 }
