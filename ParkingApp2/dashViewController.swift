f4c1ffd5950c795b5afab61783c8ce0bacf79b5c
Benson Philipose <BensonPhilipose@gmail.com>
2017-12-09 14:55:01 -0600

    UI add ons for Dash

---
+++ ParkingApp2/dashViewController.swift
@@ -0,0 +1,39 @@
+//
+//  dashViewController.swift
+//  ParkingApp2
+//
+//  Created by Benson Philipose on 12/7/17.
+//  Copyright © 2017 David Duenow. All rights reserved.
+//
+
+import UIKit
+
+class dashViewController: UIViewController {
+
+    @IBOutlet weak var profileName: UITextField!
+    @IBOutlet weak var email: UITextField!
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
