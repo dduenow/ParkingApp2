7bf53231dfe636e35db5f00fb86a1ff84aa89259
dduenow <davidduenow@mwc-122072.dhcp.missouri.edu>
2017-12-11 14:43:29 -0600

    Save Commit

--- ParkingApp2/ViewController.swift
+++ ParkingApp2/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UIViewController {
         // Do any additional setup after loading the view, typically from a nib.
     }
 
+    // Do any additional setup after loading the view, typically from a nib.
+    
+    //        Cloud.defaultPublicDatabase().retrieveObjects(with: <#T##NSPredicate#>, completionHandler: <#T##([CloudKitDecodable], Error?) -> Void#>)
+    //
+    //        Cloud.defaultPublicDatabase().userInformation { (user, error) in
+    //            user?.firstName
+    //            user?.lastName
+    //            user?.emailAddress
+    //            user.
+    //        }
+
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
