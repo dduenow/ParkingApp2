f7536909fa97b680082aa52b9813ccf412640038
AbdulEldarrat <aoe7x5@mail.missouri.edu>
2017-12-12 14:27:02 -0600

    added labels for Listings detail view to display info. Also linked listingcontroller to view correctly

--- ParkingApp2/ListingViewController.swift
+++ ParkingApp2/ListingViewController.swift
@@ -11,6 +11,11 @@ import UIKit
 class ListingViewController: UIViewController {
 
     
+    @IBOutlet weak var availability: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var size: UILabel!
+    @IBOutlet weak var listingDescription: UITextView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -25,15 +30,8 @@ class ListingViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
+    func RetrieveListings(_: [String]){
+        //call retreive listings from listings page and load the info
     }
-    */
 
 }
