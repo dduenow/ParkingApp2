80a932b8ed2b11695922b819b97aa2d971a1ddb1
sbwzq8 <sbwzq8@mail.missouri.edu>
2017-12-14 02:13:09 -0600

    last minute fix to display availability dates in listingView and listing description on listing view navigation bar

--- ParkingApp2/Listing/ListingViewController.swift
+++ ParkingApp2/Listing/ListingViewController.swift
@@ -15,10 +15,13 @@ class ListingViewController: UIViewController {
     @IBOutlet weak var price: UILabel!
     @IBOutlet weak var size: UILabel!
     @IBOutlet weak var listingDescription: UITextView!
+    @IBOutlet weak var navBar: UINavigationItem!
     
     var selectedValue: CKRecord = CKRecord(recordType: "ParkingStruct")
     override func viewDidLoad() {
         super.viewDidLoad()
+        navBar.title? = selectedValue.value(forKey: "locationDescription") as! String
+        displayAvailability()
         size.text? = selectedValue.value(forKey: "carSize") as! String
         price.text? = selectedValue.value(forKey: "price") as! String
         listingDescription.text? = selectedValue.value(forKey: "description") as! String
@@ -28,6 +31,20 @@ class ListingViewController: UIViewController {
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
+    }
+    
+    func displayAvailability() {
+        let startDate = selectedValue.value(forKey: "startDate") as! NSDate
+        let endDate = selectedValue.value(forKey: "endDate") as! NSDate
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd hh:mm"
+        let startDateString = dateFormatter.string(from:startDate as Date)
+        let endDateString = dateFormatter.string(from:endDate as Date)
+        
+        let finalDateString = startDateString + " - " + endDateString
+        availability.text = finalDateString
+        
     }
 
 }
