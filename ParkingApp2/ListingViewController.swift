b230ee1ff55c286cc3ac7b6f4d21de5217cd38f2
AbdulEldarrat <aoe7x5@mail.missouri.edu>
2017-12-13 23:44:08 -0600

    listingviewcontroller conflict

--- ParkingApp2/ListingViewController.swift
+++ ParkingApp2/ListingViewController.swift
@@ -7,29 +7,28 @@
 //
 
 import UIKit
+import CloudKit
 
 class ListingViewController: UIViewController {
-
+    
+    @IBOutlet weak var availability: UILabel!
+    @IBOutlet weak var price: UILabel!
+    @IBOutlet weak var size: UILabel!
+    @IBOutlet weak var listingDescription: UITextView!
+    
+    var selectedValue: CKRecord = CKRecord(recordType: "ParkingStruct")
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        size.text? = selectedValue.value(forKey: "carSize") as! String
+        price.text? = selectedValue.value(forKey: "price") as! String
+        listingDescription.text? = selectedValue.value(forKey: "description") as! String
+        //set attribute values to display
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
+
